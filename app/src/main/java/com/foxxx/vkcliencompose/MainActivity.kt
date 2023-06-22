2b1852d0f7132bf0b8724778af589dbc768494f1
Foxxx48 <Foxxdim@mail.ru>
2023-06-22 11:07:41 +0300

    Initial commit

---
+++ app/src/main/java/com/foxxx/vkcliencompose/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.foxxx.vkcliencompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.foxxx.vkcliencompose.ui.theme.VKClientComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VKClientComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
