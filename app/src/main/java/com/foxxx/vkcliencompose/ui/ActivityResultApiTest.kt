cec8c04addb4f69c3671a90d47d27d7405a99ab4
Foxxx48 <Foxxdim@mail.ru>
2023-07-03 15:21:44 +0300

    ActivityResultApiTest added dependencies for coil-compose

---
+++ app/src/main/java/com/foxxx/vkcliencompose/ui/ActivityResultApiTest.kt
@@ -0,0 +1,25 @@
+package com.foxxx.vkcliencompose.ui
+
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+
+@Composable
+fun ActivityResultApiTest () {
+    var imageUri by remember {
+        mutableStateOf(Uri.EMPTY)
+    }
+
+    val launcher =
+        rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = {
+            imageUri = it
+        } )
+}
