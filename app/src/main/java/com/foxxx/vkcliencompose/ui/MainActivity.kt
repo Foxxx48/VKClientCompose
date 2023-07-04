d3e9054d6c06827df9b0bb39b2a46159e24adb04
Foxxx48 <Foxxdim@mail.ru>
2023-07-04 14:48:46 +0300

    Work with VK API modified MainActivity

--- app/src/main/java/com/foxxx/vkcliencompose/ui/MainActivity.kt
+++ app/src/main/java/com/foxxx/vkcliencompose/ui/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.foxxx.vkcliencompose.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.foxxx.vkcliencompose.ui.theme.VKClientComposeTheme
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,9 +27,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    MainScreen()
+                    val authLauncher = rememberLauncherForActivityResult(
+                        contract = VK.getVKAuthActivityResultContract()
+                        ) {
+                        when (it) {
+                            is VKAuthenticationResult.Success -> {
+                                // User passed authorization
+                                Log.d("TEST_VK", "Success Auth")
+                            }
+                            is VKAuthenticationResult.Failed -> {
+                                // User didn't pass authorization
+                                Log.d("TEST_VK", "Failed Auth")
+                            }
 
-                    ActivityResultApiTest()
+                        }
+                    }
+
+                    authLauncher.launch(arrayListOf(VKScope.WALL))
+
                 }
             }
         }
